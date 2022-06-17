32bc3eed52d3d1a575a77c71b85d5d7ff0618145
sv.karushev <sv.karushev@tensor.ru>
2022-06-17 17:28:07 +0500

    Изменены ФАЙЛЫ сборки и расборки расширения КЕДО

--- cmd/ЗУП3/ЗУП3BASE.301019.build.cmd
+++ cmd/ЗУП3/ЗУП3BASE.301019.build.cmd
@@ -2,8 +2,7 @@ SET PRODUCT=saby_ce_kedo
 SET SUBSYSTEM=áìè3
 SET DESCENT=301019
 SET PLATFORM=80314
-SET PREFIX=BASE
 
 cd ../..
-v8unpack.exe -B src\%SUBSYSTEM%%PREFIX% bin\%PRODUCT%_%SUBSYSTEM%%PREFIX%_%DESCENT%.cfe --index cmd\%SUBSYSTEM%\index.json --version %PLATFORM% --descent %DESCENT%
+v8unpack.exe -B src\%SUBSYSTEM%BASE bin\%PRODUCT%_%SUBSYSTEM%BASE_%DESCENT%.cfe --index cmd\%SUBSYSTEM%\index.json --version %PLATFORM% --descent %DESCENT%
 pause
--- cmd/ЗУП3/ЗУП3CORP.301019.build.cmd
+++ cmd/ЗУП3/ЗУП3CORP.301019.build.cmd
@@ -2,8 +2,7 @@ SET PRODUCT=saby_ce_kedo
 SET SUBSYSTEM=áìè3
 SET DESCENT=301019
 SET PLATFORM=80314
-SET PREFIX=CORP
 
 cd ../..
-v8unpack.exe -B src\%SUBSYSTEM%%PREFIX% bin\%PRODUCT%_%SUBSYSTEM%%PREFIX%_%DESCENT%.cfe --index cmd\%SUBSYSTEM%\index.json --version %PLATFORM% --descent %DESCENT%
+v8unpack.exe -B src\%SUBSYSTEM%CORP bin\%PRODUCT%_%SUBSYSTEM%CORP_%DESCENT%.cfe --index cmd\%SUBSYSTEM%\index.json --version %PLATFORM% --descent %DESCENT%
 pause
